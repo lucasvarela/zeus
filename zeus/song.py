8cfa1294136020c90b02ee79f41fb29ef77e0809
Maria Camila Remolina Gutierrez <mariacamilaremolinagutierrez@gmail.com>
2018-11-11 17:54:24 +0100

    structure

---
+++ zeus/song.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Song class
+class Song:
+
+    # Initializer / Instance attributes
+    def __init__(self, fname):
+        self.fname = fname
+
+    # Instance method
+    def import_song(self):
+        """S."""
+        return True
---
+++ zeus/zeus.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from song import Song
+
+def main():
+    """Main function of zeus."""
+
+
+
+
+    print('Done.')
+
+
+if __name__ == "__main__":
+    main()
