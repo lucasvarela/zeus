b7fc98f86578c62b18115861ccc67b2effddee74
Maria Camila Remolina Gutierrez <mariacamilaremolinagutierrez@gmail.com>
2018-11-11 21:24:35 +0100

    mel spectrogram

--- zeus/song.py
+++ zeus/song.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.style as ms
+import librosa
+import librosa.display
+
+ms.use('seaborn-muted')
+
 # Song class
 class Song:
 
@@ -8,6 +16,31 @@ class Song:
         self.fname = fname
 
     # Instance method
-    def import_song(self):
-        """S."""
-        return True
+    def read_song(self):
+        """Read song."""
+
+        # Load song
+        y, sr = librosa.load(self.fname, sr=None)
+
+        print(np.shape(y))
+
+        # Let's make and display a mel-scaled power (energy-squared) spectrogram
+        S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
+
+        # Convert to log scale (dB). We'll use the peak power (max) as reference.
+        log_S = librosa.power_to_db(S, ref=np.max)
+
+        # Make a new figure
+        plt.figure(figsize=(12,4))
+
+        # Display the spectrogram on a mel scale
+        # sample rate and hop length parameters are used to render the time axis
+        librosa.display.specshow(log_S, sr=sr, x_axis='time', y_axis='mel')
+
+        # Plot params
+        plt.colorbar(format='%+02.0f dB')
+        plt.title('mel power spectrogram')
+        plt.tight_layout()
+        plt.show()
+
+
--- zeus/zeus.py
+++ zeus/zeus.py
@@ -1,13 +1,34 @@
 # -*- coding: utf-8 -*-
+
+import numpy as np
 from song import Song
 
 def main():
     """Main function of zeus."""
 
+    # Read filenames of songs
+    case1 = '../tests/case1.txt'
+    song_list = import_playlist(case1)
+    n = len(song_list)
+
+    print(n)
+    print(song_list[0])
+
+    # Create objects
+    for i in range(n):
+        song_fname = song_list[i]
+        song = Song(song_fname)
+        song.read_song()
+
+        break
 
 
+def import_playlist(path):
+    """Imports the existing playlist."""
 
-    print('Done.')
+    song_list = np.genfromtxt(path, dtype='str')
+    
+    return song_list
 
 
 if __name__ == "__main__":
